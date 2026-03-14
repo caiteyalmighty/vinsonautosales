822999761bd67a694f8468385f7ca263f15a0945
caiteyalmighty <caitrtys@gmail.com>
2026-03-14 01:52:48 -0500

    Create inventory.js

---
+++ inventory.js
@@ -0,0 +1,29 @@
+const cars = [
+    {
+        make: "Chrysler",
+        model: "Town & Country",
+        year: 2013,
+        mileage: 120000,
+        price: 6000,
+        image: "2013chryslertown&country.jpg",
+        category: "Van"
+    },
+    {
+        make: "Nissan",
+        model: "Altima 2.5s",
+        year: 2015,
+        mileage: 95000,
+        price: 8500,
+        image: "2015nissanaltima2.5s.jpg",
+        category: "Sedan"
+    },
+    {
+        make: "Buick",
+        model: "LaCrosse",
+        year: 2016,
+        mileage: 88000,
+        price: 11000,
+        image: "2016buicklacrosse.jpg",
+        category: "Sedan"
+    }
+    
